409be5c802fd7cbab0319727b6ee8a176e672ca6
user <jaglanmonu1998@gmail.com>
2020-02-13 09:53:43 +0200

    20200213_9_55

--- f5.py
+++ f5.py
@@ -1,73 +1,128 @@
 Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
 [GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
 Type "help", "copyright", "credits" or "license()" for more information.
->>> a = 5
->>> a
-5
->>> 
-========================= RESTART: /home/user/f5.py =========================
-{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/f5.py'}
-{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/f5.py', 'a': 10}
->>> type(a)
-<class 'int'>
->>> type(b)
-Traceback (most recent call last):
-  File "<pyshell#3>", line 1, in <module>
-    type(b)
-NameError: name 'b' is not defined
->>> type(c)
-Traceback (most recent call last):
-  File "<pyshell#4>", line 1, in <module>
-    type(c)
-NameError: name 'c' is not defined
->>> 
-========================= RESTART: /home/user/f5.py =========================
-{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/f5.py'}
-{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/f5.py', 'a': 10, 'b': 1.1, 'c': 'd'}
->>> type(b)
-<class 'float'>
->>> type(c)
-<class 'str'>
->>> type(a)
-<class 'int'>
->>> PRINT(4)
-Traceback (most recent call last):
-  File "<pyshell#8>", line 1, in <module>
-    PRINT(4)
-NameError: name 'PRINT' is not defined
 >>> print(4)
 4
->>> ## pracrtice P4YE
->>> type('Hello World!)
-	 
-SyntaxError: EOL while scanning string literal
->>> type('Hello World')
-	 
+>>> 5 == 5
+True
+>>> 5 == 6
+False
+>>> {}
+{}
+>>> type(True)
+<class 'bool'>
+>>> type(False)
+<class 'bool'>
+>>> x != y
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    x != y
+NameError: name 'x' is not defined
+>>> 7 =6
+SyntaxError: can't assign to literal
+>>> 7 !=6
+True
+>>> 8 > 7
+True
+>>> 4 < 6
+True
+>>> 0 = 0
+SyntaxError: can't assign to literal
+>>> 2 = 2
+SyntaxError: can't assign to literal
+>>> 2 = 3
+SyntaxError: can't assign to literal
+>>> 3 = 2
+SyntaxError: can't assign to literal
+>>> type('Hello, World!')
 <class 'str'>
 >>> type(17)
-	 
 <class 'int'>
 >>> type(3.2)
-	 
 <class 'float'>
->>> type(17)
-	 
-<class 'int'>
+>>> type('17')
+<class 'str'>
 >>> type('3.2')
-	 
 <class 'str'>
 >>> print(1,000,000)
-	 
 1 0 0
->>> 1
-	 
+>>> message = 'And now for something completely different'
+>>> n = 17
+>>> pi = 3.1415926535897931
+>>> print(n)
+17
+>>> print(pi)
+3.141592653589793
+>>> type(message)
+<class 'str'>
+>>> class 'īnt'>
+SyntaxError: invalid syntax
+>>> class 'īnt'>
+SyntaxError: invalid syntax
+>>> class 'int'>
+SyntaxError: invalid syntax
+>>> type(n)
+<class 'int'>
+>>> type(pi)
+<class 'float'>
+>>> 76trombones = 'big parade'
+SyntaxError: invalid syntax
+>>> more@ = 1000000
+SyntaxError: invalid syntax
+>>> class = 'Ādvanced Theortical Zymurgy'
+SyntaxError: invalid syntax
+>>> and       del       from      None      True
+as        elif      global    nonlocal  try
+assert    else      if        not       while
+break     except    import    or        with
+class     False     in        pass      yield
+continue  finally   is        raise     async
+def       for       lambda    return    await
+SyntaxError: invalid syntax
+>>> print(1)
 1
->>> 1+1
-	 
+>>> print(x)
+Traceback (most recent call last):
+  File "<pyshell#37>", line 1, in <module>
+    print(x)
+NameError: name 'x' is not defined
+>>> 1
+1
+>>> 2
 2
->>> x=5
-	 
+>>> x = 2
+>>> print(x)
+2
+>>> 20+32
+52
+>>> hour-1
+Traceback (most recent call last):
+  File "<pyshell#43>", line 1, in <module>
+    hour-1
+NameError: name 'hour' is not defined
+>>> 60-1
+59
+>>> 60*60+1
+3601
+>>> 1/60
+0.016666666666666666
+>>> 5**2
+25
+>>> (5+9)*(15-7)
+112
+>>> 1 = 59
+SyntaxError: can't assign to literal
+>>> 17
+17
+>>> x
+2
+>>> x+17
+19
+>>> 1+1
+2
+>>> 5
+5
+>>> x = 5
 >>> x+1
-	 
 6
->>> 
+>>>  
